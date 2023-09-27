3c08b764747177e666617ef6699bd63ea0f2b625
SvillarZ <svillarz@emeal.nttdata.com>
2023-09-27 20:41:02 -0300

    kubernetes configs

--- main.tf
+++ main.tf
@@ -18,3 +18,32 @@ resource "aws_instance" "ec2_example" {
         Name = var.instance_name
     }
 }
+
+
+# Recursos para EKS (Amazon Elastic Kubernetes Service)
+resource "aws_eks_cluster" "my_cluster" {
+    name     = "my-eks-cluster"
+    role_arn = aws_iam_role.my_eks_cluster_role.arn
+    vpc_config {
+        subnet_ids = var.subnet_ids
+    }
+}
+
+# Recurso para el bucket de S3
+resource "aws_s3_bucket" "bucket-de-almacenamiento" {
+    bucket = "bucket-de-almacenamiento"
+    acl    = "private"
+}
+
+# Configuración de Kops (para EKS)
+resource "null_resource" "kops" {
+        provisioner "local-exec" {
+        command = <<EOT
+            def clusterName = 'cluster-de-almacenamiento'
+            def awsRegion = 'us-west-2'
+
+            kops create cluster --name=${clusterName} --zones=${awsRegion}
+            kops update cluster ${clusterName} --yes
+EOT
+    }
+}
