47e58c13739d7aa8dba1bb89c494843543df299b
SvillarZ <svillarz@emeal.nttdata.com>
2023-09-29 12:47:15 -0300

    my_vpc cidr_block replaced

--- main.tf
+++ main.tf
@@ -38,7 +38,8 @@ resource "aws_instance" "ec2_example" {
 # }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+  # cidr_block = "10.0.0.0/16"
+  cidr_block = "172.31.0.0/16"
 
 }
 resource "aws_subnet" "subnet_1" {
