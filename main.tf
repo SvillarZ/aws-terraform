b9f0c2dc96efff29e717198d8ba89ddd2c8218cd
SvillarZ <svillarz@emeal.nttdata.com>
2023-09-29 12:31:56 -0300

    ingress & egress - eks_worker_sg at main.tf

--- main.tf
+++ main.tf
@@ -127,12 +127,18 @@ resource "aws_iam_policy_attachment" "my-eks_service_attachment" {
 resource "aws_security_group" "eks_worker_sg" {
   name        = "eks-worker-sg"
   description = "Security group for EKS worker nodes"
-
+  vpc_id = aws_vpc.my_vpc.id
     ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 }
 
