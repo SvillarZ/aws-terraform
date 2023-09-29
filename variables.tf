8630944e67983cde8c92a09b0355b6b16fda82a0
SvillarZ <svillarz@emeal.nttdata.com>
2023-09-28 23:56:28 -0300

    subnet id fixed

--- variables.tf
+++ variables.tf
@@ -18,6 +18,15 @@ variable "instance_name" {
     default = "ec2_example"
 }
 variable "subnet_ids" {
-    type    = list(string)
-    default = [] 
+  type    = list(string)
+  default = [
+    "subnet-00d35da20b86a8711",
+    "subnet-0e97704934613694d",
+    "subnet-041b18b73de86caa4",
+    "subnet-0bb05d8fdcc20a8ec",
+    "subnet-03157af926d4a8bf2",
+    "subnet-0ee7a2ffc3673b043",
+    "subnet-01fb3c8936fbbcadd",
+    "subnet-0726a7516752ac87a",
+  ]
 }
