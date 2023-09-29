aa6d5661a27635f88cf26eef2cad3e3fef8ce097
SvillarZ <svillarz@emeal.nttdata.com>
2023-09-29 11:27:53 -0300

    cidr block

--- main.tf
+++ main.tf
@@ -43,13 +43,13 @@ resource "aws_vpc" "my_vpc" {
 }
 resource "aws_subnet" "subnet_1" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.0.0/20"
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2a"
 
 }
 resource "aws_subnet" "subnet_2" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.0.0/24"
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-west-2b"
 }
 
